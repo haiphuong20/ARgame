4c6a750fe6608393376c9ae6aa1228519855f60b
Hoàng Hải Phương <107494429+haiphuong20@users.noreply.github.com>
2025-05-05 10:59:54 +0700

    Add files via upload

---
+++ EnablePhysicsOnEvent.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnablePhysicsOnEvent : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb; 
+
+    void Start()
+    {
+        UIButtonHandler.OnUIStartButtonClicked += StartPhysicsOnButtonClicked;
+        rb.isKinematic = true;
+    }
+
+    private void StartPhysicsOnButtonClicked()
+    {
+        rb.isKinematic = false;
+        rb.useGravity = true;
+    }
+
+    private void OnDestroy()
+    {
+        UIButtonHandler.OnUIStartButtonClicked -= StartPhysicsOnButtonClicked;
+    }
+}
